48692456d7df1b9760bfdc799481bd5f1ab5490c
Manideep <deepanddipyo@gmail.com>
2026-01-01 00:16:05 +0530

    set local https by configuration of vite config using public TLS certificate and private key, generated by local CA

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,26 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import fs from "fs"
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+export default defineConfig(({ mode }) => {
+
+  // Loads environment variables for current mdoe into node.js
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+    ],
+    server: env.VITE_IS_ENV_PROD === "true"
+      ? undefined
+      : {
+          https: {
+            key: fs.readFileSync("./certs/localhost-key.pem"),
+            cert: fs.readFileSync("./certs/localhost.pem"),
+          },
+        },
+  };
 })
