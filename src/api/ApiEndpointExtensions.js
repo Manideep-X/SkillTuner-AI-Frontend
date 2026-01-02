2ca243d202aabfb39b467e9e67613ec6a11382f2
Manideep <deepanddipyo@gmail.com>
2026-01-02 23:47:25 +0530

    endpoint extension is added to fetch the list of analysed results

--- src/api/ApiEndpointExtensions.js
+++ src/api/ApiEndpointExtensions.js
@@ -2,6 +2,7 @@ const ApiEndpointExtensions = {
     signin: "auth/signin",
     singup: "auth/signup",
     userDetails: "user/details",
+    listOfAnalyses: "resumes/job-descriptions"
 }
 
 export { ApiEndpointExtensions }
