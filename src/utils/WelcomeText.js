c2ecb1652198477ca10c4edace928c36a486afb6
Manideep <deepanddipyo@gmail.com>
2026-01-28 23:47:17 +0530

    user welcome text, and browser tab title component are added

---
+++ src/utils/WelcomeText.js
@@ -0,0 +1,7 @@
+const welcomeText = {
+    greetings: "Good to see you,",
+    followUpText: "Let's match your skills with the perfect role today!",
+    noResumeFollowUp: "Add a new resume to get started with analyzing",
+}
+
+export { welcomeText }
