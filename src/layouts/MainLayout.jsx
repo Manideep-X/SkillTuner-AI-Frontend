6b9da43add02d531ccb869714999291ed50cd939
Manideep <deepanddipyo@gmail.com>
2026-01-28 23:37:06 +0530

    fixed: issue related to opening of sidebar and it's responsiveness

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,15 +1,36 @@
 import { Outlet } from "react-router-dom"
 import SidebarNav from "../components/layout/main/SidebarNav"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { PanelRightClose, PanelRightOpen } from "lucide-react";
+
+const initialSidebarValue = () => {
+  if (typeof window === false) {
+    return;
+  }
+  return window.matchMedia("(min-width: 1024px)").matches;
+}
 
 const MainLayout = () => {
 
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(initialSidebarValue);
   const [title, setTitle] = useState("");
 
+  useEffect(() => {
+    const mediaWidth = window.matchMedia("(min-width: 1024px)");
+
+    const handler = (e) => {
+      setIsSidebarOpen(e.matches);
+    };
+
+    mediaWidth.addEventListener("change", handler);
+    
+    return () => {
+      mediaWidth.removeEventListener("change", handler);
+    }
+  }, []);
+
   return (
-    <section className="drawer lg:drawer-open">
+    <section className="drawer md:drawer-open">
       {/* Input for drawer toggle */}
       <input 
         id="my-drawer-4" 
@@ -18,8 +39,8 @@ const MainLayout = () => {
         checked={isSidebarOpen}
         onChange={() => setIsSidebarOpen(!isSidebarOpen)}
       />
-      <div className="drawer-content w-full">
-        <nav className="navbar bg-base-300">
+      <div className="drawer-content flex flex-col w-full h-screen">
+        <nav className="navbar bg-base-300 shadow-xl/95 shadow-base-300">
           <label htmlFor="my-drawer-4" aria-label="open sidebar" className="btn btn-square btn-ghost">
             {/* Sidebar toggle icon */}
             {
@@ -28,12 +49,12 @@ const MainLayout = () => {
               : <PanelRightClose />
             }
           </label>
-          <div className="px-4">{ title }</div>
+          <div className="px-4 font-bold text-lg truncate w-full">{ title }</div>
         </nav>
 
         {/* Other pages will be displayed here */}
-        <div className="overflow-x-hidden overflow-y-auto grow text-white">
-          <Outlet context={{ setTitle }} />
+        <div className="overflow-y-auto grow text-white">
+          <Outlet context={{ setTitle, isSidebarOpen }} />
         </div>
 
       </div>
