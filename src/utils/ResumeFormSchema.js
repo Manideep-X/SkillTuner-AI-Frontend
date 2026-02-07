2c0754e1196f0bc71f26e10a0d5ce12cb942faaa
Manideep <deepanddipyo@gmail.com>
2026-02-07 18:19:26 +0530

    tab title and schema resume form are improved, and landing page texts are added in utility

--- src/utils/ResumeFormSchema.js
+++ src/utils/ResumeFormSchema.js
@@ -1,6 +1,6 @@
 import z from "zod"
 
-const MAX_RESUME_SIZE = 3 * 1024 * 1024; // 3 MB
+const MAX_RESUME_SIZE = 4 * 1024 * 1024; // 5 MB
 const ACCEPTED_EXTENSIONS = ["application/pdf", "application/msword", "application/vnd.openxmlformats-officedocument.wordprocessingml.document"];
 
 const resumeFormSchema = z.object({
@@ -9,7 +9,7 @@ const resumeFormSchema = z.object({
 
     resumeFile: z.any()
         .refine((resumes) => resumes?.length === 1, "Resume is required!")
-        .refine((resumes) => resumes?.[0]?.size <= MAX_RESUME_SIZE, "Max resume size should be 3MB!")
+        .refine((resumes) => resumes?.[0]?.size <= MAX_RESUME_SIZE, "Max resume size should be 4MB!")
         .refine((resumes) => ACCEPTED_EXTENSIONS.includes(resumes?.[0]?.type), "Only .pdf, .docx, and docx file are accepted")
 });
 
