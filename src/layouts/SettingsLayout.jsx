1b2764f5b6d819aabd8506e536d889e5284f5a60
Manideep <deepanddipyo@gmail.com>
2026-02-07 18:15:32 +0530

    settings layout is modified for non-dialog component

--- src/layouts/SettingsLayout.jsx
+++ src/layouts/SettingsLayout.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { SettingPaths } from "../utils/SettingPaths"
 import Resumes from "../pages/Resumes"
 import UsernameChange from "../pages/UsernameChange"
@@ -11,20 +11,17 @@ import SettingsSectionLoading from "../components/layout/settings/SettingsSectio
 
 const SettingsLayout = () => {
 
-  const [activePage, setActivePage] = useState(null); // null means modal is closed
-  const dialogRef = useRef(null);
+  const [activePage, setActivePage] = useState(null); // null means settings menu is closed
   const location = useLocation();
   const { authStatus } = useAuth();
 
   // Hash changes
   const handleHash = () => {
     const hash = window.location.hash;
-    const modal = dialogRef.current;
 
-    // Irrelevant hash will result in closure of the modal
+    // Irrelevant hash will result in closure of the menu
     if (!hash.startsWith("#settings/")) {
       setActivePage(null);
-      if (modal?.open) modal.close();
       return;
     }
 
@@ -40,9 +37,8 @@ const SettingsLayout = () => {
     // Checks if page is present in the hash, and only then it will be displayed
     if (Object.values(SettingPaths).includes(page)) {
       setActivePage(page);
-      if (modal && !modal.open) {
-        modal.showModal();
-      }
+    } else {
+      setActivePage(null);
     }
   }
 
@@ -51,58 +47,50 @@ const SettingsLayout = () => {
     window.location.hash = "";
   }
 
-  // adding event listener for hash and modal close, removing it on unmount, and handling hash
+  // adding event listener for hash based close/open, removing it on unmount, and handling hash
   useEffect(() => {
-    const modal = dialogRef.current;
-    if (!modal) return;
 
-    // Adds event listener for hash change and closing modal
+    // Adds event listener for hash change
     window.addEventListener("hashchange", handleHash);
-    modal.addEventListener("close", closeModal);
     
     // Function call to handle hash in URL
     handleHash();
     
-    // Removes all event listener while unmount
+    // Removes the event listener while unmount
     return () => {
       window.removeEventListener("hashchange", handleHash);
-      modal.removeEventListener("close", closeModal);
     }
   }, []);
 
-  // Modal forcibly closed when react navigates
+  // Menu forcibly closed when react navigates
   useEffect(() => {
-    const modal = dialogRef.current;
-    if(!modal) return;
 
     if (location.pathname === "/signin" || location.pathname === "/signup" || location.pathname === "/") {
       setActivePage(null);
-      if (modal?.open) 
-        modal.close();
       window.location.hash = "";
     }
   }, [location.pathname]);
 
+  // Authentication safty net and retuns null for no pages
   if (authStatus === "unauthenticated") {
-    const modal = dialogRef.current;
-    if (modal?.open) 
-      modal.close();
     window.location.hash = "";
-    return "";
+    return null;
   }
 
+  if (!activePage) return null;
+
   return (
-    <dialog id="settingsModal" ref={dialogRef} className="modal modal-bottom sm:modal-middle backdrop-blur-[2px]">
+    <section className="fixed inset-0 flex items-end sm:items-center sm:justify-center bg-black/30 backdrop-blur-xs z-40">
       {
         authStatus === "loading"
         ?
-          <div className="modal-box p-10 sm:rounded-lg rounded-t-2xl w-full sm:w-11/12 md:w-196 max-w-5xl overflow-y-hidden h-130 text-white">
+          <div className="relative bg-base-200 p-10 sm:rounded-lg rounded-t-2xl w-full sm:w-11/12 md:w-196 max-w-5xl overflow-y-hidden h-130 text-white border border-accent-content/10 shadow-xl/70">
             <SettingsSectionLoading />
           </div>
         :
-          <div className="modal-box p-0 sm:rounded-lg rounded-t-2xl w-full sm:w-11/12 md:w-196 max-w-5xl overflow-y-hidden">
+          <div className="relative bg-base-200 p-0 sm:rounded-lg rounded-t-2xl w-full sm:w-11/12 md:w-196 max-w-5xl h-fit border border-accent-content/10 shadow-xl/70">
 
-            {/* Title of the setting's modal */}
+            {/* Title of the setting's menu */}
             <h3 className="flex items-center sm:text-2xl text-xl font-bold text-white px-6 py-6">
               Settings
               <ChevronRight />
@@ -116,10 +104,10 @@ const SettingsLayout = () => {
               }
             </h3>
             
-            {/* Contents of the setting's modal */}
+            {/* Contents of the setting's menu */}
             <div className="flex sm:flex-row flex-col m-0 p-0 w-full">
             
-              {/* Sidebar navigation of the setting's modal */}
+              {/* Sidebar navigation of the setting's menu */}
               <div className="bg-base-300/40 sm:w-fit w-full">
                 <nav className="menu flex sm:flex-col flex-row sm:w-fit w-full sm:justify-start justify-center sm:gap-2 gap-1">
                   <a href="#settings/resumes" className={`flex sm:gap-1.5 gap-1 sm:px-3 px-2 py-2 items-center font-semibold rounded-sm hover:bg-base-content/15 transition-all border-base-content/20 border sm:border-none ${activePage === "resumes" ? "bg-base-content/15" : ""} `}>
@@ -150,7 +138,7 @@ const SettingsLayout = () => {
               </div>
             
               {/* Different setting's section switching */}
-              <div className="grow h-130 overflow-y-auto bg-base-content/2">
+              <div className="grow h-130 overflow-auto bg-base-content/2">
                 { activePage === "resumes" && <Resumes /> }
                 { activePage === "user-name" && <UsernameChange /> }
                 { activePage === "password" && <PasswordChange /> }
@@ -159,17 +147,15 @@ const SettingsLayout = () => {
             
             </div>
             
-            {/* Close button for the setting's modal */}
-            <div className="modal-action m-0">
-              <button onClick={closeModal} className="btn btn-sm btn-circle btn-ghost size-8 absolute right-5 top-6.5 font-bold text-white">
-                <X />
-              </button>
-            </div>
+            {/* Close button for the setting's menu */}
+            <button onClick={() => (window.location.hash = "")} className="btn btn-sm btn-circle btn-ghost size-8 absolute m-0 right-5 top-6.5 font-bold text-white">
+              <X />
+            </button>
             
           </div>
       }
-    </dialog>
-  )
+    </section>
+  );
 }
 
 export default SettingsLayout
